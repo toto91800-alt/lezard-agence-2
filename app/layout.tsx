b563e53c49401c69bbf8a5208942f126bdfd3dc1
Leraillez Theo <lerailleztheo@example.com>
2025-06-17 15:34:21 -0400

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/components/providers"; // <-- ajout ici
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>{children}</Providers> {/* <- injection du provider */}
       </body>
     </html>
   );
---
+++ components/providers.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { ThemeProvider } from "next-themes"
+import type { ReactNode } from "react"
+
+type ProvidersProps = {
+  children: ReactNode
+}
+
+export function Providers({ children }: ProvidersProps) {
+  return (
+ <ThemeProvider
+  attribute="class"
+  themes={["dark", "custom"]}
+  defaultTheme="system"
+  enableSystem
+>
+      {children}
+    </ThemeProvider>
+  )
+}
