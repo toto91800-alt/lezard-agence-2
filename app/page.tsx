310b4e664ba2cfda947255a7b65fa3cb352707fb
Leraillez Theo <t.leraillez@gmail.com>
2025-06-22 10:10:04 -0400

    drone

--- app/page.tsx
+++ app/page.tsx
@@ -33,11 +33,12 @@ export default function HomePage() {
           <h2 className="text-2xl md:text-4xl lg:text-5xl font-heading font-medium !leading-snug mt-6 text-black dark:text-white">
             Donnez vie à vos projets avec style et impact
           </h2>
-  
+
         </div>
-               <CreativeTriad />
+        <CreativeTriad />
         <AlternatingCards />
-        <div className="flex flex-col items-center text-center max-w-2xl mx-auto pt-12 pb-12">
+
+        <div className="flex flex-col items-center text-center max-w-2xl mx-auto pt-12">
           <SectionBadge title="Production audiovisuelle" />
           <h2 className="text-2xl md:text-4xl lg:text-5xl font-heading font-medium !leading-snug mt-6 text-black dark:text-white">
             Donnez vie à vos projets avec style et impact
@@ -45,16 +46,19 @@ export default function HomePage() {
           <p className="text-base md:text-lg text-center text-black dark:text-accent-foreground/80 mt-6">
             Nous combinons créativité et technologie pour produire des contenus qui captivent et engagent.
           </p>
+          <img
+            src="/images/drone.png"
+            alt="Production audiovisuelle"
+            className="-mt-24 w-full max-w-md"
+          />
         </div>
         <Home />
-        <div className="flex flex-col items-center text-center max-w-2xl mx-auto pt-12 pb-12">
+         <div className="flex flex-col items-center text-center max-w-2xl mx-auto pt-12 pb-12">
           <SectionBadge title="Production audiovisuelle" />
           <h2 className="text-2xl md:text-4xl lg:text-5xl font-heading font-medium !leading-snug mt-6 text-black dark:text-white">
             Donnez vie à vos projets avec style et impact
           </h2>
-          <p className="text-base md:text-lg text-center text-black dark:text-accent-foreground/80 mt-6">
-            Nous combinons créativité et technologie pour produire des contenus qui captivent et engagent.
-          </p>
+
         </div>
         <MarqueeDemo1 />
         <Portfolio />
