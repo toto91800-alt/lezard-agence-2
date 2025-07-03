2c684234510fe330ad1fe2b81e10d79650a79844
Leraillez Theo <t.leraillez@gmail.com>
2025-07-03 11:22:09 -0400

    d

--- app/page.tsx
+++ app/page.tsx
@@ -19,16 +19,16 @@ export default function HomePage() {
         <div className="flex flex-col items-center text-center max-w-2xl mx-auto pt-12 pb-12">
           <SectionBadge title="Production audiovisuelle" />
           <h2 className="text-2xl md:text-4xl lg:text-5xl font-heading font-medium !leading-snug mt-6 text-black dark:text-white">
-            Donnez vie à vos projets avec style et impact
+            Je documente votre histoire avec passion
           </h2>
           <p className="text-base md:text-lg text-center text-black dark:text-accent-foreground/80 mt-6">
-            Nous combinons créativité et technologie pour produire des contenus qui captivent et engagent.
+           En chacun et chacune se cache une histoire à raconter. Je capture la votre et lui donne vie sous un nouveau format, la vidéo.
           </p>
         </div>
 
         <SliderProjets1 />
         <div className="flex flex-col items-center text-center max-w-2xl mx-auto pt-12 pb-12">
-          <SectionBadge title="Production audiovisuelle" />
+          <SectionBadge title="Je documente votre histoire avec passion" />
           <h2 className="text-2xl md:text-4xl lg:text-5xl font-heading font-medium !leading-snug mt-6 text-black dark:text-white">
             Donnez vie à vos projets avec style et impact
           </h2>
