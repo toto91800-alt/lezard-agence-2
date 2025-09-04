b5f491a3317562b882792eebfaa40136572aba76
Leraillez Theo <t.leraillez@gmail.com>
2025-09-04 14:14:06 +0200

    instagram

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,21 @@
 const nextConfig = {
   images: {
     remotePatterns: [
+      // ✅ ton proxy API
       {
         protocol: 'https',
         hostname: 'app.lezard-agency.com',
-        port: '',              // leave empty for default
-        pathname: '/api/ig/pdp/**', // narrow to the path you use
+        port: '',
+        pathname: '/api/ig/pdp/**',
+      },
+      // ✅ Instagram CDN (ex: scontent-cdg4-2.cdninstagram.com)
+      {
+        protocol: 'https',
+        hostname: '*.cdninstagram.com',
+        pathname: '/**',
       },
     ],
   },
 };
+
 module.exports = nextConfig;
