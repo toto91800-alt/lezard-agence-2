15fb218b3d354b485f685c1dc240f526ec0b3d4d
lucienride <lucien.maynard@gmail.com>
2025-09-16 15:30:57 +0200

    inclusion du google tag manager dans le layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Providers } from "@/components/menu/providers";
 import { NavbarDemo } from "@/components/menu/NavbarDemo";
 import MainFooter from "@/components/footer/MainFooter";
 import WhatsAppBubble from "@/components/ui/WhatsAppBubble";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,26 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Tag Manager */}
+        <Script id="gtm" strategy="beforeInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PD9XDWPC');
+          `}
+        </Script>
+
+        {/* GTM (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PD9XDWPC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Providers>
           <NavbarDemo />
           {children}
