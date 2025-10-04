4fb793198a2d22479f92f56079f2304800d68c3e
Leraillez Theo <t.leraillez@gmail.com>
2025-10-04 17:53:37 +0200

    d

--- app/blog/ [category]/[slug]/page.tsx
+++ app/blog/ [category]/[slug]/page.tsx
@@ -1,13 +1,14 @@
 interface ArticlePageProps {
-    params: { category: string; slug: string };
-  }
-  
-  export default function ArticlePage({ params }: ArticlePageProps) {
-    return (
-      <div>
-        <h1>Catégorie : {params.category}</h1>
-        <p>Article : {params.slug}</p>
-      </div>
-    );
-  }
-  
+  params: Promise<{ category: string; slug: string }>;
+}
+
+export default async function ArticlePage({ params }: ArticlePageProps) {
+  const { category, slug } = await params;
+
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-2">Catégorie : {category}</h1>
+      <p className="text-gray-700 text-lg">Article : {slug}</p>
+    </div>
+  );
+}
